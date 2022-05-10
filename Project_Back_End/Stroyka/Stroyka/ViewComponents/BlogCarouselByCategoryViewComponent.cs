22b96add6ae08119081fe1eedcd6988324246b8d
DrMadWill <nofelsalahov9@gmail.com>
2022-05-10 20:14:50 +0400

    main:Create Index > Blog SingleBlog>Blog
    HomeController > Index > Latest News and BlogController > Single > Related Posts

---
+++ Project_Back_End/Stroyka/Stroyka/ViewComponents/BlogCarouselByCategoryViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stroyka.Models;
+using Stroyka.ViewModels.ViewComponentModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stroyka.ViewComponents
+{
+    public class BlogCarouselByCategoryViewComponent: ViewComponent
+    {
+        private readonly StroykaDbContext _dbContext;
+
+        public BlogCarouselByCategoryViewComponent(StroykaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var category = await _dbContext.BlogCategories.FindAsync(id);
+            var blogs = await _dbContext.Blogs
+                .Where(dr=>dr.CategoryId == category.Id)
+                .OrderByDescending(x=>x.Date)
+                .Take(10)
+                .ToListAsync();
+            return View(blogs);
+        }
+
+    }
+}
